4ccd94c3b781810ed87427fbd3f35bea0ca5515a
nexza78 <mmm_rgb@mail.ru>
2021-04-24 14:08:06 +0300

    Create perebor.py

---
+++ perebor.py
@@ -0,0 +1,31 @@
+import itertools
+
+
+def find_cows_and_bulls(your_number, computer_number):
+    cows = 0
+    bulls = 0
+    for i1 in range(4):
+        for j1 in range(4):
+            if your_number[i1] == computer_number[j1]:
+                if i1 == j1:
+                    bulls += 1
+                else:
+                    cows += 1
+    return bulls, cows
+
+
+all_possible_numbers = set(itertools.permutations('0123456789', 4))
+
+set_of_numbers = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
+
+findNumb = ""
+
+for i in range(4):
+    findNumb += set_of_numbers.pop()
+
+for i in all_possible_numbers:
+    b, c = find_cows_and_bulls(i, findNumb)
+    print(''.join(i))
+    print(b, c)
+    if(b == 4):
+        break
